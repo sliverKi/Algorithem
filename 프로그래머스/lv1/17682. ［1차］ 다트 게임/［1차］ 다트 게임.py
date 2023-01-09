4823ee8b1a82ac45cb67385415d06621932d5a9b
sliverKi <121347506+sliverKi@users.noreply.github.com>
2023-01-09 18:13:32 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,40 @@
+
+def solution(dartResult):
+    score=[]
+    n=''
+    
+    for i in dartResult:
+        if i.isnumeric():
+            n+=i
+            
+        elif i=='S':
+            n=int(n)**1
+            score.append(n)
+            n=''
+        
+        elif i=='D':
+            n=int(n)**2
+            score.append(n)
+            n=''
+        
+        elif i=='T':
+            n=int(n)**3
+            score.append(n)
+            n=''
+        
+        elif i=='*':
+            if len(score)>1:
+                score[-2]*=2
+                score[-1]*=2
+            else: score[-1]*=2    
+            print(score)
+        elif i=='#':
+            score[-1]*=-1
+    return sum(score)    
+                
+            
+            
+            
+            
+            
+        
