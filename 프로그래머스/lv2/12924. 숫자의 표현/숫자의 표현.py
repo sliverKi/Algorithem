e01c7f05c0ec5f3c895df755454583873f3f1326
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2023-06-07 20:17:31 +0900

    [level 2] Title: 숫자의 표현, Time: 5.18 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    start=0
+
+    for i in range(1, n+1):
+        result=0
+        for j in range(i, n+1):
+            result=result+j
+            if result==n:
+                answer+=1
+                break
+            elif result>n:
+                break
+    return answer
+        
