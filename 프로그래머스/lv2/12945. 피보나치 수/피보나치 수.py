13dcb832e80541956c36e48800b8931461bcfd74
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2023-06-16 13:40:14 +0900

    [level 2] Title: 피보나치 수, Time: 460.36 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,21 +1,12 @@
-def solution(n):
-    answer = 0
-    num=[]
-    for i in range(n):
-        if i==1:
-            num.append(1)
-        else:
-            num.append(0)
-    # print(num)
-    for i in range(n):
-        # print(i)
-        num[i+2]=num[i]+num[i+1]
-        # print("num[i+2]: ", num[i+2])
-        num.append(num[i+2])
-    #     print(num)
-    # print(num)
 
-    for i in range(len(num)-(n-2)):
+def solution(n):
+    
+    num=[0,1]
+    
+    for i in range(2,n+1):
+        num.append(num[i-2]+num[i-1])
+    # print(num)
         if i==n:
-            return num[i] % 1234567
+            break
+    return num[i] % 1234567
         
