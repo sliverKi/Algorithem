b886bc58b46a964983f5b6f5f70ab0507315e323
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2023-11-16 18:37:59 +0900

    [level unrated] Title: 가위 바위 보, Time: 0.35 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
@@ -5,14 +5,11 @@ function solution(rsp) {
     let game = [{2: 0}, {0: 5}, {5: 2}]
 
     rsp=rsp.split("")
-    //console.log("rsp: ",rsp)
     for(let i=0; i<rsp.length;i++){
         for(let j=0;j<game.length;j++){
-            if(rsp[i]==Object.keys(game[j])){
+            if(rsp[i]==Object.keys(game[j]))
                 answer+=Object.values(game[j])
-            }
         }
-        
     }
     //console.log(Object.keys(game[0]))
     return answer;
