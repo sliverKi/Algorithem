25d7c7afc01f0f949dcc13ce3146ed4603fcb1de
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2023-11-20 18:47:54 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,5 @@
 function solution(num_list, n) {
-
     var answer = new Array(num_list.length/n);
-    
     for(let i=0;i<num_list.length/n;i++){ //0,1,2,3
         answer[i] = new Array(n);
         for (let j = 0; j < n; j++) {
@@ -11,8 +9,7 @@ function solution(num_list, n) {
             } else {
                 answer[i][j] = undefined;
             }
-        }
-        
+        } 
     }
     return answer;
 }
