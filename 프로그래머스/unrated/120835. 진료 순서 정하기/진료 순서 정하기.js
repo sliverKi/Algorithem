956cb4a66d04078444b33f88fb5d090774fe9874
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2023-11-22 17:31:00 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,16 +1,4 @@
 function solution(emergency) {
-    let answer = [];
-    let tmp = emergency.slice()//[3, 76, 24]
-    
-    emergency.sort((a,b)=>b-a)
-    let obj = emergency.reduce((acc, value, index) => {
-        return {...acc, [index+1]: value};
-        
-    }, {});//{ '1': 76, '2': 24, '3': 3 }
-    
-    for(let j=0; j<tmp.length;j++){
-        let key = Object.keys(obj).find(k=> obj[k] === tmp[j])
-        answer.push(parseInt(key));
-    }
-    return answer;
+    let sorted = emergency.slice().sort((a,b)=>b-a);
+    return emergency.map(v=>sorted.indexOf(v)+1);
 }
