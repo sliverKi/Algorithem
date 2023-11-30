cd264948aa145798d3386c5e00691ff4df6ebf74
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2023-11-30 23:55:22 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -2,10 +2,8 @@ function solution(keyinput, board) {
     
     let answer = [0,0];
 
-    let lrmaxMove=( board[0]-1 )/ 2//좌우로 최대한 이동할 수 있는 거리
-    let udmaxMove=( board[1]-1 )/ 2//상하로 최대한 이동할 수 있는 거리
-    let move = {'up': 1, 'down':-1, 'left':-1, 'right':1} 
-
+    let lrmaxMove=Math.floor(board[0]/ 2)//좌우로 최대한 이동할 수 있는 거리
+    let udmaxMove=Math.floor(board[1]/ 2)//상하로 최대한 이동할 수 있는 거리
     
     for(let i=0; i< keyinput.length;i++){
         if(keyinput[i]==='right'){
@@ -22,6 +20,5 @@ function solution(keyinput, board) {
                 answer[1]=answer[1]-1
         }
     }
-    //console.log("answer: ", answer)
     return answer;
 }
