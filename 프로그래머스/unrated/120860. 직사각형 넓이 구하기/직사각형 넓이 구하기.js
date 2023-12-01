17180a39859b310a73f547ec74373dac9411a9e0
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2023-12-01 17:30:41 +0900

    [level unrated] Title: 직사각형 넓이 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,39 +1,35 @@
 function solution(dots) {
     
-    //let max_x=dots[0][0] //-1
-    //let max_y=dots[0][1]//-1
     let max_x = dots[0][0]
     let min_x = dots[0][0]
     let max_y = dots[0][1]
     let min_y = dots[0][1]
     
     
-    for(let i=1;i<dots.length;i++){
-        //console.log("1:", max_x,  min_x)
-        if(dots[i][0]>max_x){
-            max_x=dots[i][0]
-            
-        }
-        if(dots[i][0]<min_x){
-            min_x = dots[i][0]
-            console.log(min_x)
-        }
-        if(dots[i][1]>max_y){
-            max_y=dots[i][1]
-            // console.log("max_y",max_y)
-        }
-        if(dots[i][1]<min_y){
-            min_y = dots[i][1]
-            // console.log("min_y", min_y)
-        }
-        
+    let w=[]
+    let h=[]
+    for (let dot of dots){
+        w.push(dot[0])
+        h.push(dot[1])
     }
-//     console.log(max_x, min_x)
-//     console.log(max_y, min_y)
     
-    // console.log("2:", max_x - min_x)
-    // console.log("2:", max_y - min_y)
-    return (max_x-min_x) * (max_y-min_y)
+    
+    return (Math.max(...w) - Math.min(...w) )*(Math.max(...h) - Math.min(...h))
+    // for(let i=1;i<dots.length;i++){
+    //     if(dots[i][0]>max_x){
+    //         max_x=dots[i][0]  
+    //     }
+    //     if(dots[i][0]<min_x){
+    //         min_x = dots[i][0]
+    //     }
+    //     if(dots[i][1]>max_y){
+    //         max_y=dots[i][1]
+    //     }
+    //     if(dots[i][1]<min_y){
+    //         min_y = dots[i][1]
+    //     } 
+    // }
+    //return (max_x-min_x) * (max_y-min_y)
     //return '';
     
 }
