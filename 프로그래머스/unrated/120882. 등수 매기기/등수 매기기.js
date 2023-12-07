0b725dee2e43bfe84b47ae4f853125309b6fab1c
Eun Gi Ko <121347506+sliverKi@users.noreply.github.com>
2023-12-07 17:18:13 +0900

    [level unrated] Title: 등수 매기기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.js
@@ -1,17 +1,16 @@
 function solution(score) {
     let answer = [];
     let rank = Array(score.length).fill(1);
-
     for(const el of score){
         const sum =el.reduce((acc, cur)=>acc+cur)
         answer.push(sum)
     }
     answer.forEach((e, i) => {
-      rank.forEach((f, j) => {
-    if (e < answer[j]) {
-      rank[i]++;
-    }
-  });
-});
+      rank.forEach((f, j) => {  
+        if (e < answer[j]) {
+            rank[i]++;
+            }
+        });
+    });
     return rank;
 }
